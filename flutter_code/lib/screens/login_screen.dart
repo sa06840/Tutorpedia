c98b8f2b5029d0dad1cd3211cd4944d863d97b93
sa06840 <sa06840@st.habib.edu.pk>
2023-03-28 00:26:54 +0500

    Implemented ListView

--- flutter_code/lib/screens/login_screen.dart
+++ flutter_code/lib/screens/login_screen.dart
@@ -24,105 +24,108 @@ class LoginScreen extends StatelessWidget {
             ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            children: <Widget>[
-              SizedBox(height: 100.0),
-              Image.asset(
-                "assets/images/logo.png",
-                scale: 2.0,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                "Tutorpedia",
-                style: TextStyle(
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 25,28,50)
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 100.0),
+                Image.asset(
+                  "assets/images/logo.png",
+                  scale: 2.0,
                 ),
-              ),
-              SizedBox(height:20.0),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(35.0, 30, 35.0, 0.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            style: BorderStyle.solid,
-                            color: Color.fromARGB(183, 25,28,50),
-                            width: 2.0,
-                          )
-                        ),
-                        hintText: "Username"
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            style: BorderStyle.solid,
-                            color: Color.fromARGB(183, 25,28,50),
-                            width: 2.0,
-                          )
-                        ),
-                        hintText: "Password",
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 25),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, MyRoutes.dashboardRoute);
-                },
-                child: Text(
-                  "Login",
+                SizedBox(height: 20.0),
+                Text(
+                  "Tutorpedia",
                   style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 25,28,50)
                   ),
                 ),
-                style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 25,28,50),
-                  padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  )
-                ),
-              ),
-              SizedBox(height: 20.0),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Don't have an Account? ",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Color.fromARGB(255, 25,28,50),
-                      )
-                    ),
-                    TextSpan(
-                      text: "Register Now!",
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 25,28,50),
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
+                SizedBox(height:20.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 30, 35.0, 0.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Color.fromARGB(183, 25,28,50),
+                              width: 2.0,
+                            )
+                          ),
+                          hintText: "Username"
+                        ),
                       ),
-                      recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.pop(context);
-                      }
-                    ),
-                  ],
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Color.fromARGB(183, 25,28,50),
+                              width: 2.0,
+                            )
+                          ),
+                          hintText: "Password",
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 25),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, MyRoutes.dashboardRoute);
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 25,28,50),
+                    padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    )
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Don't have an Account? ",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Color.fromARGB(255, 25,28,50),
+                        )
+                      ),
+                      TextSpan(
+                        text: "Register Now!",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 25,28,50),
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pop(context);
+                        }
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 50.0),
+              ],
+            ),
           ),
         ),
       ),
--- flutter_code/lib/screens/signup_screen.dart
+++ flutter_code/lib/screens/signup_screen.dart
@@ -23,93 +23,96 @@ class SignupScreen extends StatelessWidget {
             ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            children: <Widget>[
-              SizedBox(height: 100.0),
-              Image.asset(
-                "assets/images/logo.png",
-                scale: 2.0,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                "Tutorpedia",
-                style: TextStyle(
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 25,28,50)
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 100.0),
+                Image.asset(
+                  "assets/images/logo.png",
+                  scale: 2.0,
                 ),
-              ),
-              SizedBox(height:20.0),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(35.0, 30, 35.0, 0.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            style: BorderStyle.solid,
-                            color: Color.fromARGB(183, 25, 28, 50),
-                            width: 2.0,
-                          )
-                        ),
-                        hintText: "Username"
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            style: BorderStyle.solid,
-                            color: Color.fromARGB(183, 25,28,50),
-                            width: 2.0,
-                          )
-                        ),
-                        hintText: "Password",
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            style: BorderStyle.solid,
-                            color: Color.fromARGB(183, 25,28,50),
-                            width: 2.0,
-                          )
-                        ),
-                        hintText: "Student Code",
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 25),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, MyRoutes.loginRoute);
-                },
-                child: Text(
-                  "Sign Up",
+                SizedBox(height: 20.0),
+                Text(
+                  "Tutorpedia",
                   style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 25,28,50)
                   ),
                 ),
-                style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 25,28,50),
-                  padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  )
+                SizedBox(height:20.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 30, 35.0, 0.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Color.fromARGB(183, 25, 28, 50),
+                              width: 2.0,
+                            )
+                          ),
+                          hintText: "Username"
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Color.fromARGB(183, 25,28,50),
+                              width: 2.0,
+                            )
+                          ),
+                          hintText: "Password",
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Color.fromARGB(183, 25,28,50),
+                              width: 2.0,
+                            )
+                          ),
+                          hintText: "Student Code",
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 25),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, MyRoutes.loginRoute);
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 25,28,50),
+                    padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    )
+                  ),
+                ),
+                SizedBox(height: 50.0),
+              ],
+            ),
           ),
         ),
       ),
